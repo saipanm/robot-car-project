3b8a15fade931661efec71a4e6378f4156a46ead
saipanm <122227706+saipanm@users.noreply.github.com>
2024-11-02 15:40:31 +0700

    fix css

--- robot-webapp/src/App.jsx
+++ robot-webapp/src/App.jsx
@@ -1,27 +1,50 @@
 import React from 'react';
-import './dpad.css';
+import './dpad.css'
+import gifpokemon from './assets/4OKl.gif'
 
-function App() {
-  // Define handler functions for each direction
-  const handleMove = (direction) => {
-    console.log(`Moving ${direction}`);
-    // You can add any code here to send the direction to your backend or robot control API
+const App = () => {
+  //ส่งคำสั่ง control
+  const sendCommand = async (command) => {
+    try {
+      const response = await fetch('http://localhost:5000/api/robot/control', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ command }),
+      });
+
+      if (response.ok) {
+        console.log(`${command} command sent successfully`);
+      } else {
+        console.error(`Failed to send ${command} command`);
+      }
+    } catch (error) {
+      console.error(`Error sending ${command} command:`, error);
+    }
   };
 
   return (
-    <div >
-      <h1 style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>Robot Webapp</h1>
+    <div>
+      <h2 className="title">
+        <img src={gifpokemon} alt="Loading" className="title-gif" />
+        RoboSphere
+      </h2>     
+      <div style={{display: 'flex', justifyContent:'center'}}>
       <div className="set blue">
         <div className="d-pad">
-          <a className="up" onClick={() => handleMove('up')} href="#"/>
-          <a className="right" onClick={() => handleMove('right')} href="#"/>
-          <a className="down" onClick={() => handleMove('down')} href="#"/>
-          <a className="left" onClick={() => handleMove('left')} href="#"/>
-         
+          <a className="up" onClick={() => sendCommand('up')} href="#"/>
+          <a className="right" onClick={() => sendCommand('right')} href="#"/>
+          <a className="down" onClick={() => sendCommand('down')} href="#"/>
+          <a className="left" onClick={() => sendCommand('left')} href="#"/>
         </div>
+      </div>
       </div>
     </div>
   );
-}
+
+
+
+};
 
 export default App;
